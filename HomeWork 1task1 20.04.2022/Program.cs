8acfef69f4bc6a834f3227241019c446e5266574
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-20 15:10:55 +0400

    added homework

---
+++ HomeWork 1task1 20.04.2022/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HomeWork_1task1_20._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Ismayil" ,"Ibrahimli", 20, "Male", true, 58 );
+            Console.WriteLine(student.GetInfo());
+        }
+
+    }
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public string Gender;
+        public bool isGraduated;
+        public byte Point;
+        public Student(string name, string surname, byte age, string gender, bool graduated, byte point)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Gender = gender;
+            Point = point;
+            isGraduated = graduated;
+            if( graduated = true)
+            {
+                Console.WriteLine("Congratulations! You're graduated :)");
+            }
+            if (point >= 80)
+            {
+                Console.WriteLine("You have chance for a second exam");
+            }
+            else
+            {
+                Console.WriteLine("You failed the exam");
+            }
+        }
+        public string GetInfo()
+        {
+
+            return $"Name:{Name} Surname: {Surname} Age: {Age} Gender: {Gender} Point: {Point} Is Graduated: {isGraduated} ";
+        }
+    }
+
+}
+           
+        
+    
